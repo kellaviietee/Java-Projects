b5ffd6bd44bb4539a70fa0f7f40db9f9008c355f
kellaviietee <l.niilo@hotmail.com>
2022-02-07 09:39:06 +0200

    LOTR

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
@@ -9,94 +9,93 @@ public class Ring {
         NENYA,
         OTHER
     }
-    enum Material{
+
+    enum Material {
         GOLD,
         SILVER,
         BRONZE,
         PLASTIC,
         DIAMOND
     }
+
     Type type;
     Material material;
 
-    public Ring(Type type, Material material){
-    this.type = type;
-    this.material = material;
+    public Ring(Type type, Material material) {
+        this.type = type;
+        this.material = material;
     }
 
-    public Type getType(){
+    public Type getType() {
         return type;
     }
 
-    public Material getMaterial(){
+    public Material getMaterial() {
         return material;
     }
-public static class Person{
+
+    public static class Person {
         String race;
         String name;
         Ring currentRing;
 
-    public Person(String race, String name){
-        this.race = race;
-        this. name = name;
+        public Person(String race, String name) {
+            this.race = race;
+            this.name = name;
+        }
+
+        public void setRing(Ring ring) {
+            currentRing = ring;
+        }
+
+        public String isSauron() {
+            if (Objects.equals(name, "Sauron") && currentRing.type == Type.THE_ONE && currentRing.material == Material.GOLD) {
+                return "Affirmative";
+            } else if (Objects.equals(name, "Sauron") && currentRing.type == Type.THE_ONE && currentRing.material != Material.GOLD) {
+                return "No, the ring is fake!";
+            } else if (!Objects.equals(name, "Sauron") && currentRing.type == Type.THE_ONE && currentRing.material == Material.GOLD) {
+                return "No, he just stole the ring";
+            } else if (Objects.equals(name, "Sauron") && currentRing.type != Type.THE_ONE || currentRing == null) {
+                return "No, but he's claiming to be";
+            } else {
+                return "No";
+            }
+        }
+
     }
-    public void setRing(Ring ring){
-    currentRing = ring;
-    }
-
-    public String isSauron(){
-        if(Objects.equals(name, "Sauron") && currentRing.type == Type.THE_ONE && currentRing.material == Material.GOLD){
-            return "Affirmative";
-        }
-        else if(Objects.equals(name, "Sauron") && currentRing.type == Type.THE_ONE && currentRing.material != Material.GOLD){
-            return  "No, the ring is fake!";
-        }
-        else if(!Objects.equals(name, "Sauron") && currentRing.type == Type.THE_ONE && currentRing.material == Material.GOLD) {
-            return "No, he just stole the ring";
-        }
-        else if(Objects.equals(name, "Sauron") && currentRing.type != Type.THE_ONE || currentRing == null) {
-            return "No, but he's claiming to be";
-        }
-        else{
-            return "No";
-        }
-    }
-
-}
 
 
-
-    public static void main(String[] args){
+        public static void main(String[] args) {
 
 // LOTR simplified play through
-        Ring theRing = new Ring(Type.THE_ONE, Material.GOLD);
-        Person sauron = new Person("Maiar", "Sauron");
-        sauron.setRing(theRing);
+            Ring theRing = new Ring(Ring.Type.THE_ONE, Ring.Material.GOLD);
+            Ring.Person sauron = new Ring.Person("Maiar", "Sauron");
+            sauron.setRing(theRing);
 // after some 4000 years, Gollum got the ring
-        Person gollum = new Person("Hobbit", "Gollum");
+            Ring.Person gollum = new Ring.Person("Hobbit", "Gollum");
 // let's remove ring from Sauron
-        sauron.setRing(null);
-        gollum.setRing(theRing);
+            sauron.setRing(null);
+            gollum.setRing(theRing);
 // after about 500 years, Bilbo got the ring
-        Person bilbo = new Person("Hobbit", "Bilbo Baggins");
-        gollum.setRing(null);
-        bilbo.setRing(theRing);
+            Ring.Person bilbo = new Ring.Person("Hobbit", "Bilbo Baggins");
+            gollum.setRing(null);
+            bilbo.setRing(theRing);
 // after 60 years, Frodo got the ring
-        Person frodo = new Person("Hobbit", "Frodo Baggins");
-        bilbo.setRing(null);
-        frodo.setRing(theRing);
+            Ring.Person frodo = new Ring.Person("Hobbit", "Frodo Baggins");
+            bilbo.setRing(null);
+            frodo.setRing(theRing);
 // check Sauron
-        Ring fakeOne = new Ring(Type.THE_ONE, Material.PLASTIC);
-        sauron.setRing(fakeOne);
-        System.out.println(sauron.isSauron()); // No, the ring is fake!
-        System.out.println(frodo.isSauron()); // No, he just stole the ring
-        Ring nenya = new Ring(Type.NENYA, Material.DIAMOND);
-        sauron.setRing(nenya);
-        System.out.println(sauron.isSauron()); // No, but he's claiming to be
-        frodo.setRing(nenya);
-        System.out.println(frodo.isSauron()); // No
-        sauron.setRing(theRing);
-        System.out.println(sauron.isSauron()); // Affirmative
+            Ring fakeOne = new Ring(Ring.Type.THE_ONE, Ring.Material.PLASTIC);
+            sauron.setRing(fakeOne);
+            System.out.println(sauron.isSauron()); // No, the ring is fake!
+            System.out.println(frodo.isSauron()); // No, he just stole the ring
+            Ring nenya = new Ring(Ring.Type.NENYA, Ring.Material.DIAMOND);
+            sauron.setRing(nenya);
+            System.out.println(sauron.isSauron()); // No, but he's claiming to be
+            frodo.setRing(nenya);
+            System.out.println(frodo.isSauron()); // No
+            sauron.setRing(theRing);
+            System.out.println(sauron.isSauron()); // Affirmative
 
+        }
     }
-}
