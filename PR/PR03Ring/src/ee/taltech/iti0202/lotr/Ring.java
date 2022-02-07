05518233b0ffc74909e5bb04b36eeb46bfc58f90
kellaviietee <l.niilo@hotmail.com>
2022-02-07 10:23:19 +0200

    LOTR

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
@@ -33,7 +33,6 @@ public class Ring {
     public Material getMaterial() {
         return material;
     }
-
     public static class Person {
         String race;
         String name;
@@ -65,37 +64,39 @@ public class Ring {
     }
 
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
 // LOTR simplified play through
-            Ring theRing = new Ring(Ring.Type.THE_ONE, Ring.Material.GOLD);
-            Ring.Person sauron = new Ring.Person("Maiar", "Sauron");
-            sauron.setRing(theRing);
+        Ring theRing = new Ring(Ring.Type.THE_ONE, Ring.Material.GOLD);
+        Ring.Person sauron = new Person("Maiar", "Sauron");
+        sauron.setRing(theRing);
 // after some 4000 years, Gollum got the ring
-            Ring.Person gollum = new Ring.Person("Hobbit", "Gollum");
+        Ring.Person gollum = new Person("Hobbit", "Gollum");
 // let's remove ring from Sauron
-            sauron.setRing(null);
-            gollum.setRing(theRing);
+        sauron.setRing(null);
+        gollum.setRing(theRing);
 // after about 500 years, Bilbo got the ring
-            Ring.Person bilbo = new Ring.Person("Hobbit", "Bilbo Baggins");
-            gollum.setRing(null);
-            bilbo.setRing(theRing);
+        Ring.Person bilbo = new Person("Hobbit", "Bilbo Baggins");
+        gollum.setRing(null);
+        bilbo.setRing(theRing);
 // after 60 years, Frodo got the ring
-            Ring.Person frodo = new Ring.Person("Hobbit", "Frodo Baggins");
-            bilbo.setRing(null);
-            frodo.setRing(theRing);
+        Ring.Person frodo = new Person("Hobbit", "Frodo Baggins");
+        bilbo.setRing(null);
+        frodo.setRing(theRing);
 // check Sauron
-            Ring fakeOne = new Ring(Ring.Type.THE_ONE, Ring.Material.PLASTIC);
-            sauron.setRing(fakeOne);
-            System.out.println(sauron.isSauron()); // No, the ring is fake!
-            System.out.println(frodo.isSauron()); // No, he just stole the ring
-            Ring nenya = new Ring(Ring.Type.NENYA, Ring.Material.DIAMOND);
-            sauron.setRing(nenya);
-            System.out.println(sauron.isSauron()); // No, but he's claiming to be
-            frodo.setRing(nenya);
-            System.out.println(frodo.isSauron()); // No
-            sauron.setRing(theRing);
-            System.out.println(sauron.isSauron()); // Affirmative
+        Ring fakeOne = new Ring(Ring.Type.THE_ONE, Ring.Material.PLASTIC);
+        sauron.setRing(fakeOne);
+        System.out.println(sauron.isSauron()); // No, the ring is fake!
+        System.out.println(frodo.isSauron()); // No, he just stole the ring
+        Ring nenya = new Ring(Ring.Type.NENYA, Ring.Material.DIAMOND);
+        sauron.setRing(nenya);
+        System.out.println(sauron.isSauron()); // No, but he's claiming to be
+        frodo.setRing(nenya);
+        System.out.println(frodo.isSauron()); // No
+        sauron.setRing(theRing);
+        System.out.println(sauron.isSauron()); // Affirmative
 
-        }
     }
+
+
+}
