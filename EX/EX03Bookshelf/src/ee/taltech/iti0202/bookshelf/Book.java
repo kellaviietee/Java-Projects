521106bfc7b3603c65a64923d0c452b304f9a3e4
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-09 15:11:31 +0200

    Bookshelf restart

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -1,93 +1,26 @@
 package ee.taltech.iti0202.bookshelf;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-
 public class Book {
-    String title;
-    String author;
-    int yearOfPublishing;
-    int price;
-    Person owner;
-    int bookId;
-    static int id = 0;
-    static List<Book> books = new ArrayList<>();
+
+    public static int availableId = 0;
+    public String title;
+    public String author;
+    public int yearOfPublishing;
+    public int price;
 
     public static int getAndIncrementNextId() {
-        return id + 1;
+        int currentId = availableId;
+        availableId += 1;
+        return currentId;
     }
-    public static Book of(String title, String author, int yearOfPublishing, int price){
-        for(Book book : books){
-            if(Objects.equals(book.getTitle(), title) && Objects.equals(book.getAuthor(), author)
-                    && book.getYearOfPublishing() == yearOfPublishing && book.getPrice() == price){
-                return book;
-            }
-        }
-        Book newBook = new Book(title,author,yearOfPublishing,price);
-        books.add(newBook);
-        return newBook;
-    }
-    public static Book of(String title, int price){
-        if(books.size() == 0){
-            return null;
-        }
-        String author = books.get(books.size()-1).getAuthor();
-        int yearOfPublishing = books.get(books.size()-1).getYearOfPublishing();
-        for(Book book : books){
-            if(Objects.equals(book.getTitle(), title) && book.getPrice() == price
-            && book.getYearOfPublishing() == yearOfPublishing && Objects.equals(book.getAuthor(), author)){
-                return book;
-            }
-            }
-        Book newBook = new Book(title,author,yearOfPublishing,price);
-        books.add(newBook);
-        return newBook;
-    }
-    public static List<Book> getBooksByOwner(Person owner){
-        return owner.getBooks();
-    }
-    public static boolean removeBook(Book book){
-        if (book == null){
-            return false;
-        }
-        for(Book testBook : books){
-            if(Objects.equals(testBook.getTitle(), book.getTitle()) && Objects.equals(testBook.getAuthor(), book.getAuthor())
-            && testBook.getYearOfPublishing() == book.getYearOfPublishing() && testBook.getPrice() == book.getPrice()) {
-                if(book.getOwner() != null){
-                    Person owner = book.getOwner();
-                    owner.sellBook(book);
-                }
-                books.remove(book);
-                return true;
-            }
-        }
-        return false;
-    }
-    public static List<Book> getBooksByAuthor(String author){
-        List<Book> booksByAuthor = new ArrayList<>();
-        for(Book book : books){
-            if(Objects.equals(book.getAuthor().toLowerCase(), author.toLowerCase())){
-                booksByAuthor.add(book);
-            }
-        }
-        return booksByAuthor;
-    }
-
 
     public Book(String title, String author, int yearOfPublishing, int price) {
-        this.bookId = id;
-        id = id + 1;
         this.title = title;
         this.author = author;
         this.yearOfPublishing = yearOfPublishing;
         this.price = price;
-        this.owner = null;
     }
 
     public String getTitle() {
-
         return title;
     }
 
@@ -96,12 +29,11 @@ public class Book {
     }
 
     public int getYearOfPublishing() {
-
         return yearOfPublishing;
     }
 
     public Person getOwner() {
-        return owner;
+        return null;
     }
 
     public int getPrice() {
@@ -109,19 +41,11 @@ public class Book {
     }
 
     public int getId() {
-        return bookId;
+        return availableId;
     }
 
     public boolean buy(Person buyer) {
-        if(getOwner() == buyer || buyer.getMoney() < getPrice()){
-            return false;
-        }
-        Person currentOwner = getOwner();
-        if(currentOwner != null) {
-            currentOwner.sellBook(this);
-        }
-        buyer.buyBook(this);
-        return true;
+        return false;
     }
 
 }
--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
@@ -1,17 +1,12 @@
 package ee.taltech.iti0202.bookshelf;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Person {
-    String name;
-    int money;
-    List<Book> ownedBooks;
-    
+    public String name;
+    public int money;
+
     public Person(String name, int money) {
         this.name = name;
         this.money = money;
-        this.ownedBooks = new ArrayList<>();
     }
 
     public int getMoney() {
@@ -23,29 +18,10 @@ public class Person {
     }
 
     public boolean buyBook(Book book) {
-        if (book != null) {
-            if(getMoney() >= book.getPrice() && book.owner == null){
-                book.owner = this;
-                money -= book.getPrice();
-                ownedBooks.add(book);
-                return true;
-            }
-        }
         return false;
     }
 
     public boolean sellBook(Book book) {
-        if(book != null){
-            if(book.getOwner() == this){
-                money += book.getPrice();
-                book.owner = null;
-                ownedBooks.remove(book);
-                return true;
-            }
-        }
         return false;
-    }
-    public List<Book> getBooks(){
-        return ownedBooks;
     }
 }
