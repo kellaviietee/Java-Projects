1f7e4daa72f939b92e360022244bd29a7bfd862b
kellaviietee <l.niilo@hotmail.com>
2022-02-09 11:44:50 +0200

    getBookByOwner.

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -1,4 +1,8 @@
 package ee.taltech.iti0202.bookshelf;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Book {
     String title;
     String author;
@@ -7,9 +11,41 @@ public class Book {
     Person owner;
     int bookId;
     static int id = 0;
+    static List<Book> books;
     public static int getAndIncrementNextId() {
         return id;
     }
+    public static Book of(String title, String author, int yearOfPublishing, int price){
+        for(Book book : books){
+            if(Objects.equals(book.getTitle(), title) && Objects.equals(book.getAuthor(), author)
+                    && book.getYearOfPublishing() == yearOfPublishing && book.getPrice() == price){
+                return book;
+            }
+        }
+        Book newBook = new Book(title,author,yearOfPublishing,price);
+        books.add(newBook);
+        return newBook;
+    }
+    public static Book of(String title, int price){
+        if(books.size() == 0){
+            return null;
+        }
+        String author = books.get(books.size()-1).getAuthor();
+        int yearOfPublishing = books.get(books.size()-1).getYearOfPublishing();
+        for(Book book : books){
+            if(Objects.equals(book.getTitle(), title) && book.getPrice() == price
+            && book.getYearOfPublishing() == yearOfPublishing && Objects.equals(book.getAuthor(), author)){
+                return book;
+            }
+            }
+        Book newBook = new Book(title,author,yearOfPublishing,price);
+        books.add(newBook);
+        return newBook;
+    }
+    public static List<Book> getBooksByOwner(Person owner){
+
+    }
+
 
     public Book(String title, String author, int yearOfPublishing, int price) {
         this.bookId = id;
