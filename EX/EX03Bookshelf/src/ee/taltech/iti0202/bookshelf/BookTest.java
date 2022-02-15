02a869d7d14f8d7907de90862a7ef98007e49f4b
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-15 14:01:16 +0200

    TestingStart

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/BookTest.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/BookTest.java
@@ -10,16 +10,34 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
  * Tester for Bookshelf.
  */
 class BookTest {
-    static final int TEST_YEAR = 2010;
-    static final int TEST_PRICE = 20;
-    static final int FIRST_ID = 0;
-    static final int SECOND_ID = 1;
-    static final int NO_MONEY = 0;
+    public static final int TEST_YEAR = 2010;
+    public static final int TEST_PRICE = 20;
+    public static final int FIRST_ID = 0;
+    public static final int SECOND_ID = 1;
+    public static final int NO_MONEY = 0;
+    public static final int ENOUGH_MONEY = 200;
+    /**
+     * Test if the First available ID is 0
+     */
+    @Test
+    void testFirstId() {
+        assertEquals(FIRST_ID, Book.availableId);
+    }
 
+    /**
+     * Test available ID after one Book
+     */
+
+    @Test
+    void testIdAfterBook(){
+        Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
+                "Indrek Hargla", TEST_YEAR, TEST_PRICE);
+        assertEquals(SECOND_ID,Book.availableId);
+    }
     /**
      * Test if a single book creation works.
      */
-    @Test
+    /*@Test
     void createSingleBook() {
         Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
                 "Indrek Hargla", TEST_YEAR, TEST_PRICE);
@@ -30,28 +48,12 @@ class BookTest {
         assertEquals(TEST_PRICE, testBook.getPrice());
     }
 
-    /**
-     * Test if the First available ID is 0
-     */
-    @Test
-    void testFirstId() {
-        assertEquals(FIRST_ID, Book.availableId - 1);
-    }
 
-    /**
-     * Test available ID after one Book
-     */
-    @Test
-    void testIdAfterBook(){
-        Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
-                "Indrek Hargla", TEST_YEAR, TEST_PRICE);
-        assertEquals(SECOND_ID,Book.availableId - 1);
-    }
     /**
      * Test Person buying null book.
      */
-    @Test
-    void TestPersonBuyNullBook(){
+    /*@Test
+    void testPersonBuyNullBook(){
         Person testPerson = new Person("Lauri",100);
         assertFalse(testPerson.buyBook(null));
 
@@ -60,12 +62,43 @@ class BookTest {
     /**
      * Test Person buying book with no money.
      */
-    @Test
-    void TestPersonBuyBookNoMoney(){
+    /*@Test
+    void testPersonBuyBookNoMoney(){
         Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
                 "Indrek Hargla", TEST_YEAR, TEST_PRICE);
         Person testPerson = new Person("Lauri",NO_MONEY);
         assertEquals(false,testPerson.buyBook(testBook));
     }
+    /**
+     * Test Person buying book.
+     */
+    /*@Test
+    void testPersonSellBook(){
+        Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
+                "Indrek Hargla", TEST_YEAR, TEST_PRICE);
+        Person testPerson = new Person("Lauri",ENOUGH_MONEY);
+        assertEquals(true,testPerson.buyBook(testBook));
+    }
+    /**
+     * Test Book owner after buying
+     */
+    /*@Test
+    void testGetBookOwner(){
+        Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
+                "Indrek Hargla", TEST_YEAR, TEST_PRICE);
+        Person testPerson = new Person("Lauri",ENOUGH_MONEY);
+        testPerson.buyBook(testBook);
+        assertEquals(testPerson,testBook.getOwner());
+    }
+    /**
+     * Test Book ID after a book is spawned.
+     */
+    /*@Test
+    void testGetBookID(){
+        Book testBook = new Book("Apteeker Melchior ja Oleviste mõistatus",
+                "Indrek Hargla", TEST_YEAR, TEST_PRICE);
+        assertEquals(0,testBook.getId());
+    }
+     */
 }
 
