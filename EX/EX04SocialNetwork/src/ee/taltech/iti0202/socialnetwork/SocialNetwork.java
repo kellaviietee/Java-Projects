998e7775120ed4b9e9e5680fc2e4f3cc4bdfafe8
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-17 16:35:46 +0200

    EX04

--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
@@ -8,7 +8,7 @@
     import java.util.Set;
 
     public class SocialNetwork {
-        private static Set<Group> groups = new HashSet<>();
+        private static final Set<Group> groups = new HashSet<>();
 
         public void registerGroup(Group group) {
             groups.add(group);
