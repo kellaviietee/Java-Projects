26713435990c35ce0441fc1f0998d121cbba3fad
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-18 16:21:35 +0200

    LX01Style

--- LX/LX01Stream/src/ee/taltech/iti0202/stream/KittenStatistics.java
+++ LX/LX01Stream/src/ee/taltech/iti0202/stream/KittenStatistics.java
@@ -29,6 +29,10 @@ public class KittenStatistics {
 
     }
 
+    /**
+     * Find oldest kitten.
+     * @return oldest kitten.
+     */
     public Optional<Kitten> findOldestKitten() {
         if (kittens.size() == 0) {
             return Optional.empty();
@@ -37,6 +41,10 @@ public class KittenStatistics {
         }
     }
 
+    /**
+     * Find youngest kittens.
+     * @return youngest kittens.
+     */
     public List<Kitten> findYoungestKittens() {
         Integer youngestAge = kittens.stream()
                 .mapToInt(Kitten::getAge)
@@ -47,12 +55,23 @@ public class KittenStatistics {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Find kittens by gender.
+     * @param gender Gender of requested kitten.
+     * @return All kittens with the requested gender.
+     */
     public List<Kitten> findKittensAccordingToGender(Kitten.Gender gender) {
         return kittens.stream()
                 .filter(kitten -> kitten.getGender() == gender)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Find kittens within appropriate ages.
+     * @param minAge Minimum age of the kitten.
+     * @param maxAge Maximum age of the kitten.
+     * @return List of all the kittens between min and max ages.
+     */
     public List<Kitten> findKittensBetweenAges(int minAge, int maxAge) {
         return kittens.stream()
                 .filter(kitten -> kitten.getAge() >= minAge)
@@ -60,6 +79,11 @@ public class KittenStatistics {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Find the first kitten with specified name.
+     * @param givenName Kittens name.
+     * @return First kitten with that name.
+     */
     public Optional<Kitten> findFirstKittenWithGivenName(String givenName) {
         return kittens.stream()
                 .filter(kitten -> kitten.getName().equalsIgnoreCase(givenName))
@@ -67,6 +91,10 @@ public class KittenStatistics {
 
     }
 
+    /**
+     * Sort kittens by age, with younger ones first.
+     * @return list of sorted kittens.
+     */
     public List<Kitten> kittensSortedByAgeYoungerFirst() {
         return kittens.stream()
                 .sorted(Comparator.comparing(Kitten::getAge))
@@ -74,6 +102,10 @@ public class KittenStatistics {
 
     }
 
+    /**
+     * Sort kittens by age, with older ones being first.
+     * @return list of sorted kittens.
+     */
     public List<Kitten> kittensSortedByAgeOlderFirst() {
         return kittens.stream()
                 .sorted(Comparator.comparing(Kitten::getAge).reversed())
