3f2cadd2c3130801e5f2954008fca190ab256660
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-21 22:03:33 +0200

    TK

--- TK/TK0/src/ee/taltech/iti0202/tk0/Test01EvenOdd.java
+++ TK/TK0/src/ee/taltech/iti0202/tk0/Test01EvenOdd.java
@@ -3,7 +3,7 @@ package ee.taltech.iti0202.tk0;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TK {
+public class Test01EvenOdd {
 
     public List<Integer> evenOdd(List<Integer> nums){
         List<Integer> evenOdd = new ArrayList<>();
@@ -18,16 +18,5 @@ public class TK {
         }
         return evenOdd;
     }
-
-    public static void main(String[]args){
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
-        TK tk = new TK();
-        System.out.println(tk.evenOdd(numbers).toString());
-    }
+    
 }
