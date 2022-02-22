1ed81133c2794bf02fb9d2cdbda21bdb381e64c9
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-22 11:42:23 +0200

    TK

---
+++ TK/TK0/src/ee/taltech/iti0202/tk0/Cat.java
@@ -0,0 +1,52 @@
+package ee.taltech.iti0202.tk0;
+
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+    private int age;
+    private String color;
+
+    public Cat(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Cat(String name){
+        this.name = name;
+        this.age = 0;
+        this.color = "All cat's colors are beautiful....meow";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + name + " " + age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && name.equals(cat.name) && color.equals(cat.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, color);
+    }
+}
---
+++ TK/TK0/src/ee/taltech/iti0202/tk0/Person.java
@@ -0,0 +1,31 @@
+package ee.taltech.iti0202.tk0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private List<Cat> cats = new ArrayList<>();
+
+    public boolean addCat(Cat cat){
+        if(cats.contains(cat) || cat == null){
+            return false;
+        } else{
+          cats.add(cat);
+          return true;
+        }
+    }
+
+    public List<Cat> getCats() {
+        return cats;
+    }
+    public boolean sellCat(Person sellTo, Cat cat){
+        if(sellTo == null || cat == null || sellTo.getCats().contains(cat) || !cats.contains(cat)){
+            return false;
+        }
+        else{
+            sellTo.addCat(cat);
+            cats.remove(cat);
+            return true;
+        }
+    }
+}
