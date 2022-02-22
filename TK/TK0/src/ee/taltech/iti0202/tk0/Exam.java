fd5f92b26b3e08b11b4b7ac21a897d8042d9685d
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-22 11:00:11 +0200

    TK

--- TK/TK0/src/ee/taltech/iti0202/tk0/Exam.java
+++ TK/TK0/src/ee/taltech/iti0202/tk0/Exam.java
@@ -40,7 +40,17 @@ public class Exam {
      * loneSum(3, 3, 3) → 0
      */
     public static int loneSum(int a, int b, int c) {
-        return 0;
+        int sum = 0;
+        if(a != b && a!=c){
+            sum += a;
+        }
+        if (b!= c && b!=a){
+            sum +=b;
+        }
+        if (c !=a && c!= b){
+            sum += c;
+        }
+        return sum;
     }
 
 
