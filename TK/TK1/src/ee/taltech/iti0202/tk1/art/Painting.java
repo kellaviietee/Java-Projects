8cdd6500695582df4ccee43b025f3b9c822e7b00
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-23 14:43:25 +0200

    TK

---
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
@@ -0,0 +1,13 @@
+package ee.taltech.iti0202.tk1.art;
+
+public class Painting {
+
+    private final String title;
+    private final String author;
+
+    public Painting(String title, String author){
+
+        this.title = title;
+        this.author = author;
+    }
+}
