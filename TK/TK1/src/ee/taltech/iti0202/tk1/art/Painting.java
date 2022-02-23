4de96c142031ac51b717b3eeefff08c5a6e87302
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-23 14:49:45 +0200

    TK

--- TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
@@ -2,12 +2,43 @@ package ee.taltech.iti0202.tk1.art;
 
 public class Painting {
 
-    private final String title;
-    private final String author;
+    private  String title;
+    private String author;
 
     public Painting(String title, String author){
 
         this.title = title;
         this.author = author;
+    }
+
+    public Painting(String title){
+
+        this.title = title;
+        this.author = "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String author){
+        this.author = author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        if (!author.equals("")) {
+            return "This is a painting named " + title + " and made by " + author + ".";
+        } else {
+            return "This is a painting named " + title + " and made by an unknown artist.";
+        }
     }
 }
