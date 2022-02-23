733f9edbb1f2c9e4feb53a2f551c2bfe16d1bebd
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-23 15:08:32 +0200

    TK

--- TK/TK1/src/ee/taltech/iti0202/tk1/Exam.java
+++ TK/TK1/src/ee/taltech/iti0202/tk1/Exam.java
@@ -70,13 +70,12 @@ public class Exam {
 
      Modify and return the given map as follows: if the keys "a" and "b" are both in the map
      and have equal values, remove them both.
-     
      mapAB({"a": "aaa", "b": "aaa", "c": "cake"}) → {"c": "cake"}
      mapAB({"a": "aaa", "b": "bbb"}) → {"a": "aaa", "b": "bbb"}
      mapAB({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
      */
     public static Map<String, String> mapAB(Map<String, String> map) {
-        if(map.containsKey("a") && map.containsKey("b")){
+        if (map.containsKey("a") && map.containsKey("b")) {
             if(map.get("a").equals(map.get("b"))){
                 map.remove("a");
                 map.remove("b");
