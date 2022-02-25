4a3d19a7cbda39df2c64fad301980edd73c32f87
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-25 13:24:32 +0200

    MagicOrbsStyleFixes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/exceptions/CannotFixException.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/exceptions/CannotFixException.java
@@ -2,15 +2,21 @@ package ee.taltech.iti0202.mysticorbs.exceptions;
 
 import ee.taltech.iti0202.mysticorbs.oven.Oven;
 
-public class CannotFixException extends Exception{
+public class CannotFixException extends Exception {
 
     private final Oven oven;
     private final Reason reason;
 
-    public enum Reason{
+    public enum Reason {
         IS_NOT_BROKEN, FIXED_MAXIMUM_TIMES, NOT_ENOUGH_RESOURCES
     }
-    public CannotFixException(Oven oven, Reason reason){
+
+    /**
+     * CannotFixException constructor.
+     * @param oven Oven this exception is about.
+     * @param reason Why this oven can not be fixed.
+     */
+    public CannotFixException(Oven oven, Reason reason) {
         this.oven = oven;
         this.reason = reason;
     }
