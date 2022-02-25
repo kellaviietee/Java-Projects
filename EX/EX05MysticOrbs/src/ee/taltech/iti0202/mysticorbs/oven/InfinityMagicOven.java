fdaeecb95d73da396005aabcb26e1697fb0fde25
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-25 12:35:37 +0200

    MagicOrbsStyleFixes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/InfinityMagicOven.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/InfinityMagicOven.java
@@ -2,7 +2,12 @@ package ee.taltech.iti0202.mysticorbs.oven;
 
 import ee.taltech.iti0202.mysticorbs.storage.ResourceStorage;
 
-public class InfinityMagicOven extends MagicOven{
+public class InfinityMagicOven extends MagicOven {
+    /**
+     * InfinityMagicOven Constructor.
+     * @param name name of the Oven.
+     * @param resourceStorage Storage where resources come from.
+     */
     public InfinityMagicOven(String name, ResourceStorage resourceStorage) {
         super(name, resourceStorage);
     }
