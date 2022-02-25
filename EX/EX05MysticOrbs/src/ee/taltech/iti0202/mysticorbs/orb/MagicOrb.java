261dd4b8496ee869b86480df6bb2d0ee53bc33cc
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-25 12:24:19 +0200

    MagicOrbsStyleFixes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/orb/MagicOrb.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/orb/MagicOrb.java
@@ -1,14 +1,17 @@
 package ee.taltech.iti0202.mysticorbs.orb;
 
-public class MagicOrb extends Orb{
-
+public class MagicOrb extends Orb {
+    /**
+     * MagicOrb constructor.
+     * @param creator Oven's name that this orb came out of.
+     */
     public MagicOrb(String creator) {
         super(creator);
     }
     @Override
-    public void charge(String resource, int amount){
+    public void charge(String resource, int amount) {
         if (!resource.equalsIgnoreCase("dust")) {
-            energy += resource.trim().length() * 2 * Integer.max(0,amount);
+            energy += resource.trim().length() * 2 * Integer.max(0, amount);
         }
     }
 
