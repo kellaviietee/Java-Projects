0e3faf256c5a43cb21529335a0f8c0648aa71a46
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-25 13:22:07 +0200

    MagicOrbsStyleFixes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/factory/OrbFactory.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/factory/OrbFactory.java
@@ -18,33 +18,50 @@ public class OrbFactory {
     private final List<Orb> orbs = new ArrayList<>();
     private final List<Oven> notFixableOvens = new ArrayList<>();
 
-    public OrbFactory(ResourceStorage resourceStorage){
+    /**
+     * OrbFactory constructor.
+     * @param resourceStorage Resource storage used by the factory.
+     */
+    public OrbFactory(ResourceStorage resourceStorage) {
 
         this.resourceStorage = resourceStorage;
     }
-    public void addOven(Oven oven){
+
+    /**
+     * Add ovens to the Factory.
+     * @param oven Oven to be added onto the Factory floor.
+     */
+    public void addOven(Oven oven) {
         ovens.add(oven);
     }
 
+    /**
+     * Returns currently produced orbs and then resets them.
+     * @return Orbs that were produced.
+     */
     public List<Orb> getAndClearProducedOrbsList() {
         List<Orb> allOrbs = new ArrayList<>(orbs);
         orbs.clear();
         return allOrbs;
     }
 
-    public List<Oven> getOvens(){
+    public List<Oven> getOvens() {
         return ovens;
     }
 
+    /**
+     * Produce Orbs, if the Oven is broken try to fix it. if it is not fixable then add it to not fixable ovens list.
+     * @return number of Orbs produced.
+     */
     public int produceOrbs() {
         List<Orb> craftedOrbs = new ArrayList<>();
-        for(Oven oven:ovens){
-            if(oven.isBroken()){
+        for (Oven oven:ovens) {
+            if (oven.isBroken()) {
                 if (oven.getClass() == (MagicOven.class) || oven.getClass() == SpaceOven.class) {
                     try {
                         ((Fixable) oven).fix();
-                    } catch (CannotFixException ex){
-                        if(ex.getReason() == CannotFixException.Reason.FIXED_MAXIMUM_TIMES) {
+                    } catch (CannotFixException ex) {
+                        if (ex.getReason() == CannotFixException.Reason.FIXED_MAXIMUM_TIMES) {
                             notFixableOvens.add(oven);
                         }
                     }
@@ -58,9 +75,15 @@ public class OrbFactory {
         orbs.addAll(craftedOrbs);
         return craftedOrbs.size();
     }
+
+    /**
+     * Produce Orbs.
+     * @param cycles How many times do the Ovens go.
+     * @return number of orbs created.
+     */
     public int produceOrbs(int cycles) {
         int orbNum = 0;
-        for(int i = 0; i < cycles; i++){
+        for (int i = 0; i < cycles; i++) {
             int addedOrbs = produceOrbs();
             orbNum += addedOrbs;
         }
@@ -71,6 +94,9 @@ public class OrbFactory {
         return notFixableOvens;
     }
 
+    /**
+     * Remove Ovens that can not produce more orbs.
+     */
     public void getRidOfOvensThatCannotBeFixed() {
         List<Oven> notFixableOvens = getOvensThatCannotBeFixed();
         for (Oven oven:notFixableOvens) {
@@ -78,6 +104,10 @@ public class OrbFactory {
         }
         notFixableOvens.clear();
     }
+
+    /**
+     * Sort the Ovens in a suitable manner.
+     */
     public void optimizeOvensOrder() {
         ovens.sort(Oven::compareTo);
     }
