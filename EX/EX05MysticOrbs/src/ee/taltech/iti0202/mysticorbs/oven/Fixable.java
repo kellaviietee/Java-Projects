bcecc3ffdac35dddf34754c3b25d777489dd493d
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-25 12:33:21 +0200

    MagicOrbsStyleFixes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/Fixable.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/Fixable.java
@@ -3,6 +3,15 @@ package ee.taltech.iti0202.mysticorbs.oven;
 import ee.taltech.iti0202.mysticorbs.exceptions.CannotFixException;
 
 public interface Fixable {
+    /**
+     * Try to fix an Oven.
+     * @throws CannotFixException if it cannot be fixed throw this error.
+     */
     void fix() throws CannotFixException;
+
+    /**
+     * How many times has the oven been fixed.
+     * @return times Oven has been fixed.
+     */
     int getTimesFixed();
 }
