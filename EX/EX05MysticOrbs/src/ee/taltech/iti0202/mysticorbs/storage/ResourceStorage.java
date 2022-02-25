cdbffa0818480eee3cd1c809f7840fe03dda4db0
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-02-25 12:15:49 +0200

    MagicOrbsStyleFixes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/storage/ResourceStorage.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/storage/ResourceStorage.java
@@ -28,11 +28,22 @@ public class ResourceStorage {
         }
     }
 
+    /**
+     *  Check how much of the resource is in the storage.
+     * @param resource Name of the resource.
+     * @return amount in the storage.
+     */
     public int getResourceAmount(String resource) {
         String normResource = resource.toLowerCase();
         return resources.getOrDefault(normResource, 0);
     }
 
+    /**
+     * Check if the storage has enough of the resource needed.
+     * @param resource Name of the resource being checked.
+     * @param amount How much of the resource is being taken out.
+     * @return If there is enough resources or not.
+     */
     public boolean hasEnoughResource(String resource, int amount) {
         String normResource = resource.toLowerCase();
         if (amount < 1) {
@@ -42,13 +53,19 @@ public class ResourceStorage {
         } else return amount <= resources.get(normResource);
     }
 
+    /**
+     * Take resources from the storage to be used.
+     * @param resource Name of the resource taken.
+     * @param amount How much is it taken.
+     * @return If the resource taking was successful.
+     */
     public boolean takeResource(String resource, int amount) {
-        if (hasEnoughResource(resource,amount)) {
+        if (hasEnoughResource(resource, amount)) {
             String normResource = resource.toLowerCase();
             Integer currentAmount = resources.get(normResource);
             int newAmount = currentAmount - amount;
             if (newAmount != 0) {
-            resources.replace(normResource,newAmount);
+            resources.replace(normResource, newAmount);
             } else {
                 resources.remove(normResource);
             }
