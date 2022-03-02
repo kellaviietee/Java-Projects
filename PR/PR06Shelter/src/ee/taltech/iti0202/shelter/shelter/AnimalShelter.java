c8a79f8976e6c3fcd9e1622fe601f54a610a630d
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-03-02 10:25:28 +0200

    AnimalShelter

--- PR/PR06Shelter/src/ee/taltech/iti0202/shelter/shelter/AnimalShelter.java
+++ PR/PR06Shelter/src/ee/taltech/iti0202/shelter/shelter/AnimalShelter.java
@@ -35,7 +35,7 @@ public class AnimalShelter {
                     .filter(animal -> Objects.equals(animal.getColor(), color))
                     .toList();
             for(Animal animal : filteredAnimals) {
-                if (shelterAnimals.size() < count) {
+                if (shelterAnimals.size() < count - 1) {
                     shelterAnimals.add(animal);
                 }
             }
