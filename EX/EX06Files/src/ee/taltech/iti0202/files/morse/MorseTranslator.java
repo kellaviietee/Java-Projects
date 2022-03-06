e41a241611b25ab331adfba2d553c49aced84edf
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-03-06 21:01:20 +0200

    Files

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -52,7 +52,7 @@ public class MorseTranslator {
         System.out.println(Arrays.toString(words));
         StringBuilder sentence = new StringBuilder();
         for (String letter : words) {
-            if (letter.startsWith("\t") || letter.startsWith(" ")) {
+            if (letter.startsWith("\t") || letter.startsWith("\s")) {
                 sentence.append(" ");
             }
             sentence.append(reverseMorseCodes.get(letter.trim()));
