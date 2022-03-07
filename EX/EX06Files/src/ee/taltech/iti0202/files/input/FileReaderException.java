91826a319a7df137386dbd89b32841efcebb3fe2
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-03-07 11:09:01 +0200

    Files

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
@@ -1,12 +1,16 @@
 package ee.taltech.iti0202.files.input;
 
-public class FileReaderException extends RuntimeException{
+public class FileReaderException extends RuntimeException {
 
     private final String message;
     private final Throwable reason;
 
+    /**
+     * Filereader exception constructor
+     * @param message Message to go with FileReader.
+     * @param reason Type of exception.
+     */
     public FileReaderException(String message, Throwable reason) {
-
         this.message = message;
         this.reason = reason;
     }
