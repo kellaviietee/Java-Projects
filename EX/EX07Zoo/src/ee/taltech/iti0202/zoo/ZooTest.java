f8a50f63e79cee7200604884bdb187a1a9509ce5
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-03-11 17:27:23 +0200

    Zoo

--- EX/EX07Zoo/src/ee/taltech/iti0202/zoo/ZooTest.java
+++ EX/EX07Zoo/src/ee/taltech/iti0202/zoo/ZooTest.java
@@ -11,24 +11,29 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZooTest {
+    static private final int TEST_HUNGER_DAYS = 6;
+    static private final int TEST_HUNGER_DAYS_1 = 20;
+    static private final int TEST_HUNGER_DAYS_2 = 21;
+
     Animal animal1 = new Animal("Eedi", "phrphr", 4, AnimalType.MAMMAL);
-    Animal animal2 = new Animal("Žanna", "hiss", 21, AnimalType.MAMMAL);
+    Animal animal2 = new Animal("Žanna", "hiss", TEST_HUNGER_DAYS_2, AnimalType.MAMMAL);
     Animal animal3 = new Animal("Vaak", "kraak", 2, AnimalType.BIRD);
-    Animal animal4 = new Animal("Kora", "mullmull", 6, AnimalType.FISH);
+    Animal animal4 = new Animal("Kora", "mullmull", TEST_HUNGER_DAYS, AnimalType.FISH);
     Animal animal5 = new Monkey("Patu", 5);
     Animal animal6 = new Lamb("Lambi");
     Animal animal7 = new Turtle("Qua", 3);
-    Animal animal8 = new Animal("Xenia", "hiss", 20, AnimalType.AMPHIBIAN);
+    Animal animal8 = new Animal("Xenia", "hiss", TEST_HUNGER_DAYS_1, AnimalType.AMPHIBIAN);
     Animal animal9 = new Animal("Villi", "viuh", 3, AnimalType.BIRD);
-    Caretaker caretaker1 = new Caretaker("Aadam",new ArrayList<AnimalType>(Arrays.asList(AnimalType.MAMMAL,
+
+    Caretaker caretaker1 = new Caretaker("Aadam", new ArrayList<AnimalType>(Arrays.asList(AnimalType.MAMMAL,
             AnimalType.BIRD)));
-    Caretaker caretaker2 = new Caretaker("Anna",new ArrayList<AnimalType>(Arrays.asList(AnimalType.MAMMAL,
+    Caretaker caretaker2 = new Caretaker("Anna", new ArrayList<AnimalType>(Arrays.asList(AnimalType.MAMMAL,
             AnimalType.FISH)));
-    Caretaker caretaker3 = new Caretaker("Aime",new ArrayList<AnimalType>(Arrays.asList(AnimalType.AMPHIBIAN,
+    Caretaker caretaker3 = new Caretaker("Aime", new ArrayList<AnimalType>(Arrays.asList(AnimalType.AMPHIBIAN,
             AnimalType.FISH)));
     @Test
     void testMonkeyNoises() {
-        Animal testMonkey = new Monkey("Manney",5);
+        Animal testMonkey = new Monkey("Manney", 5);
         List<String> monkeyNoises = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             String monkeyNoise = testMonkey.getNoise();
@@ -45,8 +50,8 @@ class ZooTest {
      */
     @Test
     void testGetAllAnimalsInZoo() {
-        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5
-                , animal6, animal7, animal8, animal9));
+        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5,
+                animal6, animal7, animal8, animal9));
         Zoo testZoo = new Zoo();
         testZoo.addAllAnimalsToZoo(testAnimals);
         assertEquals(testAnimals, testZoo.getAnimalsInZoo());
@@ -88,20 +93,20 @@ class ZooTest {
      */
     @Test
     void testHungryAnimalsInZoo() {
-        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5
-                , animal6, animal7, animal8, animal9));
+        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5,
+                animal6, animal7, animal8, animal9));
         Zoo testZoo = new Zoo();
         testZoo.addAllAnimalsToZoo(testAnimals);
         for (int days = 0; days < 10; days++) {
             testZoo.nextDay();
         }
-        List<Animal> testHungryAnimals = new ArrayList<>(Arrays.asList(animal1,animal3,animal4
-                ,animal5,animal7,animal9));
-        assertEquals(testHungryAnimals,testZoo.hungryAnimals());
+        List<Animal> testHungryAnimals = new ArrayList<>(Arrays.asList(animal1, animal3, animal4,
+                animal5, animal7, animal9));
+        assertEquals(testHungryAnimals, testZoo.hungryAnimals());
         testZoo.addCaretakerToZoo(caretaker1);
         caretaker1.addAnimalsToFeed(testZoo.hungryAnimals());
         List<Animal> newHungryAnimals = new ArrayList<>(Arrays.asList(animal4, animal7));
-        assertEquals(newHungryAnimals,testZoo.hungryAnimals());
+        assertEquals(newHungryAnimals, testZoo.hungryAnimals());
 
     }
 
@@ -110,8 +115,8 @@ class ZooTest {
      */
     @Test
     void testNoisesInTheZoo() {
-        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5
-                , animal6, animal7, animal8, animal9));
+        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5,
+                animal6, animal7, animal8, animal9));
         List<Caretaker> testCaretakers = new ArrayList<>(List.of(caretaker1, caretaker2, caretaker3));
         Zoo testZoo = new Zoo();
         testZoo.addAllCaretakersToZoo(testCaretakers);
@@ -119,22 +124,22 @@ class ZooTest {
         for (int days = 0; days < 10; days++) {
             testZoo.nextDay();
         }
-        String testNoises = "Eedi MAMMAL:\n" +
-                "Žanna MAMMAL:hiss\n" +
-                "Vaak BIRD:\n" +
-                "Kora FISH:\n" +
-                "Patu MAMMAL:BANANA\n" +
-                "Lambi MAMMAL:Mää\n" +
-                "Qua AMPHIBIAN:\n" +
-                "Xenia AMPHIBIAN:hiss\n" +
-                "Villi BIRD:";
-        assertEquals(testNoises,testZoo.animalNoises());
+        String testNoises = "Eedi MAMMAL:\n"
+                + "Žanna MAMMAL:hiss\n"
+                + "Vaak BIRD:\n"
+                + "Kora FISH:\n"
+                + "Patu MAMMAL:BANANA\n"
+                + "Lambi MAMMAL:Mää\n"
+                + "Qua AMPHIBIAN:\n"
+                + "Xenia AMPHIBIAN:hiss\n"
+                + "Villi BIRD:";
+        assertEquals(testNoises, testZoo.animalNoises());
     }
 
     @Test
     public void testMostEffectiveCaretaker() {
-        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5
-                , animal6, animal7, animal8, animal9));
+        List<Animal> testAnimals = new ArrayList<>(Arrays.asList(animal1, animal2, animal3, animal4, animal5,
+                animal6, animal7, animal8, animal9));
         List<Caretaker> testCaretakers = new ArrayList<>(List.of(caretaker1, caretaker2, caretaker3));
         Zoo testZoo = new Zoo();
         testZoo.addAllCaretakersToZoo(testCaretakers);
@@ -144,7 +149,7 @@ class ZooTest {
         }
         Caretaker mostEfficientCaretaker = caretaker1;
 
-        assertEquals(mostEfficientCaretaker,testZoo.mostEffectiveCaretaker());
+        assertEquals(mostEfficientCaretaker, testZoo.mostEffectiveCaretaker());
 
     }
 
