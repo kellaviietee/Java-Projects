24f89fa7aaef358dd258612507783889a3477171
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-03-19 16:55:00 +0200

    Coffee

--- EX/EX08Coffee/src/coffee/Main.java
+++ EX/EX08Coffee/src/coffee/Main.java
@@ -1,21 +1,7 @@
 package coffee;
 
-import coffee.builders.AutoCoffeeMachineBuilder;
-import coffee.builders.CapsuleMachineBuilder;
-import coffee.builders.CoffeeMachineBuilder;
-import coffee.drinks.DrinkType;
-import coffee.exceptions.DrinkTypeException;
-import coffee.exceptions.TrashContainerException;
-import coffee.exceptions.WaterTankException;
-import coffee.machines.AutoCoffeeMachine;
-import coffee.machines.CoffeeMachine;
-import coffee.trashcontainer.TrashContainer;
-import coffee.watertank.WaterTank;
 
 public class Main {
-    public static void main(String[]args) throws TrashContainerException, WaterTankException, DrinkTypeException {
-        CapsuleMachineBuilder builder = new CapsuleMachineBuilder();
-        CoffeeMachine capsuleMachine = builder.build();
-        System.out.println(capsuleMachine.getMachineType());
+    public static void main(String[]args) {
         }
     }
--- EX/EX08Coffee/src/coffee/builders/CoffeeMachineBuilder.java
+++ EX/EX08Coffee/src/coffee/builders/CoffeeMachineBuilder.java
@@ -2,7 +2,7 @@ package coffee.builders;
 
 import coffee.machines.CoffeeMachine;
 
-public class CoffeeMachineBuilder extends MachineBuilder<CoffeeMachine,CoffeeMachineBuilder> {
+public class CoffeeMachineBuilder extends MachineBuilder<CoffeeMachine, CoffeeMachineBuilder> {
     @Override
     public CoffeeMachine build() {
         return new CoffeeMachine(trashContainer, waterTank);
