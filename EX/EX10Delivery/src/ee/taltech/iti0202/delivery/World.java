47070f19655414e974ee833d5031803a6d14691b
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2022-04-09 16:33:54 +0300

    testing

--- EX/EX10Delivery/src/ee/taltech/iti0202/delivery/World.java
+++ EX/EX10Delivery/src/ee/taltech/iti0202/delivery/World.java
@@ -1,11 +1,19 @@
 package ee.taltech.iti0202.delivery;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class World {
+    Map<String, Location> worldLocations = new HashMap<>();
 
     public Optional<Location> addLocation(String name, List<String> otherLocations, List<Integer> distances) {
+        if (!worldLocations.containsKey(name)) {
+            Location newLocation = new Location(name);
+            worldLocations.put(name, newLocation);
+            return Optional.of(newLocation);
+        }
         return Optional.empty();
     }
 
